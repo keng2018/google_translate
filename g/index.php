e6f14b962112ea9dea9a72b11ce79b0a4af6bd89
weihua <175990271@qq.com>
2018-12-25 18:05:27 +0800

    1

---
+++ g/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once ('vendor/autoload.php');
+use \Statickidz\GoogleTranslate;
+
+$source = 'zh-cn';
+$target = 'en';
+$text = $_GET['q'];
+
+$trans = new GoogleTranslate();
+$result = $trans->translate($source, $target, $text);
+
+$source = 'en';
+$target = 'zh-cn';
+$trans = new GoogleTranslate();
+$res = $trans->translate($source,$target,$result);
+
+echo $res;
+?>
